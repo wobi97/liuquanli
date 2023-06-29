716fb7ec2f367850569baa52060aeb1323028d2a
“liuql@edityworld.com” <liuql@edityworld.com>
2023-06-29 14:12:38 +0800

    Initial commit

---
+++ src/main/java/com/lql/liuquanli/LiuquanliApplication.java
@@ -0,0 +1,13 @@
+package com.lql.liuquanli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiuquanliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiuquanliApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lql/liuquanli/LiuquanliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lql.liuquanli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiuquanliApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
